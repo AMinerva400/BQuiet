699dc564657897be304b78f1c9b299cb37124f9b
ANTHONY <aminerva400@gmail.com>
2019-04-15 13:33:04 -0400

    Updating Databases

--- app/src/main/java/minerva/anthony/bquiet/ContactsDatabase.java
+++ app/src/main/java/minerva/anthony/bquiet/ContactsDatabase.java
@@ -3,7 +3,7 @@ package minerva.anthony.bquiet;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities={User.class}, version=1, exportSchema = false)
+@Database(entities={User.class}, version=2, exportSchema = false)
 public abstract class ContactsDatabase extends RoomDatabase {
 
     public abstract UserDao userDao();
