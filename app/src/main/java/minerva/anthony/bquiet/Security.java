aa6c80a8825984cab7562955fb4eb08cd56067aa
Alex Rufer <arufer2@pride.hofstra.edu>
2019-04-15 00:52:02 -0400

    added encoding to mantain size

--- app/src/main/java/minerva/anthony/bquiet/Security.java
+++ app/src/main/java/minerva/anthony/bquiet/Security.java
@@ -1,10 +1,10 @@
 package minerva.anthony.bquiet;
 
-import android.util.Base64;
 import android.util.Log;
 
 import java.security.Key;
 
+import java.util.Base64;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
@@ -17,36 +17,44 @@ public class Security {
 
     protected String encryptionType = "AES/ECB/PKCS5Padding";
 
-    public Security() {
-        try{
-            ecipher = Cipher.getInstance(encryptionType);
-            dcipher = Cipher.getInstance(encryptionType);
-            byte[] encodedKey = Base64.decode("4xK23JJqwAyyA36M0pdiBw==", Base64.DEFAULT);
-            secretKey = new SecretKeySpec(encodedKey, 0, encodedKey.length, "AES");
-        }catch(Exception exc){
-            Log.e("ERROR", exc.toString());
-        }
-    }
+    public class Security {
+        private static SecretKey secretKey;
+        private static Cipher ecipher;
+        private static Cipher dcipher;
 
-    public String encrypt(String plainField) {
-        try{
-            ecipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            byte[] encryptedField = ecipher.doFinal(plainField.getBytes());
-            return encryptedField.toString();
-        }
-        catch(Exception e){
-            Log.e("ERROR", e.toString());
-        }
-    }
+        protected String encryptionType = "AES/ECB/PKCS5Padding";
 
-    public String decrypt(String encryptedField) {
-        try{
-            dcipher.init(Cipher.DECRYPT_MODE, secretKey);
-            byte[] plainField = dcipher.doFinal(encryptedField.getBytes());
-            return plainField.toString();
+        public Security() {
+            try{
+                ecipher = Cipher.getInstance(encryptionType);
+                dcipher = Cipher.getInstance(encryptionType);
+                byte[] encodedKey = Base64.getDecoder().decode("4xK23JJqwAyyA36M0pdiBw==");
+                secretKey = new SecretKeySpec(encodedKey, 0, encodedKey.length, "AES");
+            }catch(Exception exc){
+                Log.e("ERROR", exc.toString());
+            }
         }
-        catch(Exception e){
-            Log.e("ERROR", e.toString());
+
+        public String encrypt(String plainField) {
+            try{
+                ecipher.init(Cipher.ENCRYPT_MODE, secretKey);
+                byte[] encryptedField = ecipher.doFinal(plainField.getBytes());
+                return Base64.getEncoder().encodeToString(encryptedField);
+            }
+            catch(Exception e){
+                Log.e("ERROR", e.toString());
+            }
+        }
+
+        public String decrypt(String encryptedField) {
+            try
+            {
+                dcipher.init(Cipher.DECRYPT_MODE, secretKey);
+                byte[] plainField = dcipher.doFinal(Base64.getDecoder().decode(encryptedField));
+                return plainField.toString();
+            }
+            catch(Exception e){
+                Log.e("ERROR", e.toString());
+            }
         }
     }
-}
